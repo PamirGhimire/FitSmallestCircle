f748b961c9e6b0e42d16bfd6a701a7663a2e0845
Pamir Ghimire <pamirghimire@gmail.com>
2020-10-11 12:29:49 -0700

    move commandline args parsing function to separate file

--- start.py
+++ start.py
@@ -1,40 +1,10 @@
-import sys, getopt
+import sys
 
+from CLIHelper import _parseArgs
 from DataGenerator import Random2DPointsSetGenerator
 from FitCircle import FitCircleTo2DPoints
 from Visualization import CreateFigure, OpenSavedFigure
 
-def _parseArgs(argv):
-    useStubData = False
-    useExternalImpl = True
-
-    stubArg = ""
-    implArg = ""
-    try:
-        opts, args = getopt.getopt(argv,"hi:o:",["useStubData=","useExternalImpl="])
-    except getopt.GetoptError:
-        print ('usage : start.py --useStubData <true/false> --useExternalImpl <true/false>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print ('usage : start.py --useStubData <true/false> --useExternalImpl <true/false>')
-            sys.exit()
-        elif opt in ("--useStubData"):
-            stubArg = arg
-        elif opt in ("--useExternalImpl"):
-            implArg = arg
-
-    validArgs = ["true", "false", ""]
-    if(stubArg in validArgs and implArg in validArgs):
-        if stubArg=="true":
-            useStubData = True
-        if implArg=="false":
-            useExternalImpl = False        
-    else:
-        print ('usage : start.py --useStubData <true/false> --useExternalImpl <true/false>')
-        sys.exit(2)
-
-    return useStubData, useExternalImpl
 
 def main(argv):
     useStubData, useExternalImpl = _parseArgs(argv)
